d578e1c9823802021651e078b0375976d9edbb90
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-04 21:32:12 +0600

    updated

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-627y6i*e@!+tk_v6j3jn0odhs7m1a11$qq&rk(we6$6rxagq(r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
