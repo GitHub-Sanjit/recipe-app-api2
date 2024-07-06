bd8c31aa6f520f1f8b59b228d8d916e26c5180fb
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-06 10:10:34 +0600

    Django Admin

--- app/core/models.py
+++ app/core/models.py
@@ -25,7 +25,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -44,5 +44,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-
-
