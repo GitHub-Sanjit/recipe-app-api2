048d12764ceefe1e89d701bbbfb74522929047ef
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-09 17:47:05 +0600

    Recipe Image API

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,8 @@ from drf_spectacular.views import (  # type: ignore
 )
 from django.contrib import admin  # type: ignore
 from django.urls import path, include  # type: ignore
+from django.conf.urls.static import static  # type: ignore
+from django.conf import settings  # type: ignore
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -36,3 +38,9 @@ urlpatterns = [
     path("api/user/", include("user.urls")),
     path("api/recipe/", include("recipe.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
