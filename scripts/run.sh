39444fc3cd4eb01cff2908a4fb00e9af36b1641e
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-12 18:40:29 +0600

    updated12

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,4 @@ python manage.py wait_for_db    #.sh $DJANGO_DB_HOST
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+uwsgi --ini uwsgi.ini
