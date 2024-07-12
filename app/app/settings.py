f98af43c9fa85e53ed9b90edd6eb8a9caf9d0a3a
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-12 20:53:07 +0600

    updated20

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,12 +28,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = ["https://recipe-app-api2.onrender.com", "localhost", "127.0.0.1"]
-ALLOWED_HOSTS.extend(
-    filter(
-        None,
-        os.environ.get("ALLOWED_HOSTS", "").split(","),
-    )
-)
+# ALLOWED_HOSTS.extend(
+#     filter(
+#         None,
+#         os.environ.get("ALLOWED_HOSTS", "").split(","),
+#     )
+# )
 
 # Application definition
 
